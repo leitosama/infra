53ce477c1c9bd31e5b859a1f1122a2837998a23d
leitosama <fs.leito@gmail.com>
2025-01-13 16:43:01 +0700

    [vpn] Multiple instances

--- vpn/vpn.tf
+++ vpn/vpn.tf
@@ -10,7 +10,7 @@ terraform {
 # 1. Some inputs here
 # 1.1. Variables
 variable "LOCATION" {
-  description = "Location code in vultr. See https://api.vultr.com/v2/regions for details"
+  description = "Location codes list in vultr. See https://api.vultr.com/v2/regions for details"
 }
 
 variable "VULTR_API_KEY" {
@@ -33,25 +33,26 @@ provider "vultr" {
 
 # 2. Create vultr shared CPU instance
 resource "vultr_instance" "vpn" {
+    for_each = toset(var.LOCATION)
     # plan: https://api.vultr.com/v2/plans
     # vc2-1c-0.5gb-free - free tier
     # vc2-1c-1gb - min plan
     plan = "vc2-1c-1gb"
     # region: https://api.vultr.com/v2/regions
-    region = var.LOCATION
+    region = each.value
     # os_id: https://api.vultr.com/v2/os
     # 391 - CoreOS Stable
     os_id = 391
-    label = "${var.LOCATION}"
+    label = "${each.value}"
     tags = ["vpn"]
-    hostname = "${var.LOCATION}"
+    hostname = "${each.value}"
     enable_ipv6 = false
     disable_public_ipv4 = false
     backups = "disabled"
     ddos_protection = false
     activation_email = false
     user_data = templatefile("${path.module}/ignition.tftpl.json", {
-      hostname = var.LOCATION
+      hostname = each.value
       os_user = var.USERNAME
       ssh_key = var.SSH_KEY
     })   
@@ -60,11 +61,12 @@ resource "vultr_instance" "vpn" {
 # 3. Output 
 output "instance_details" {
   description = "Details of the Instance"
-  value = {
-    ip = vultr_instance.vpn.main_ip
-    os = vultr_instance.vpn.os
-    region = vultr_instance.vpn.region
-    status = vultr_instance.vpn.status
-    hostname = vultr_instance.vpn.hostname
+  value = { for k,v in vultr_instance.vpn : k=> {
+      ip = v.main_ip
+      os = v.os
+      region = v.region
+      status = v.status
+      hostname = v.hostname
+    }
   }
 }
