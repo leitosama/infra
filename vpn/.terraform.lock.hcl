caa9411ea7965c36e954fe0fa0a265ec64b8cf83
leitosama <fs.leito@gmail.com>
2025-01-13 15:24:47 +0700

    [vpn] Terraforming...

---
+++ vpn/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/vultr/vultr" {
+  version     = "2.23.1"
+  constraints = "2.23.1"
+  hashes = [
+    "h1:VNpHOLXDPr/hNlsMoHFtUPjBhJnEuUxo5ywd5UeenX8=",
+  ]
+}
--- vpn/vpn.tf
+++ vpn/vpn.tf
@@ -17,6 +17,14 @@ variable "VULTR_API_KEY" {
   description = "VULTR API key"
 }
 
+variable "USERNAME" {
+  description = "OS username"
+}
+
+variable "SSH_KEY" {
+  description = "OS SSH key"
+}
+
 provider "vultr" {
     api_key = var.VULTR_API_KEY
 }
@@ -24,23 +32,39 @@ provider "vultr" {
 # 1.2. Current data in Cloud
 
 # 2. Create vultr shared CPU instance
-resource "vultr_instance" "my_instance" {
+resource "vultr_instance" "vpn" {
     # plan: https://api.vultr.com/v2/plans
-    plan = "vc2-1c-2gb"
+    # vc2-1c-0.5gb-free - free tier
+    # vc2-1c-1gb - min plan
+    plan = "vc2-1c-1gb"
     # region: https://api.vultr.com/v2/regions
     region = var.LOCATION
     # os_id: https://api.vultr.com/v2/os
     # 391 - CoreOS Stable
     os_id = 391
-    label = "vpn-${var.LOCATION}"
+    label = "${var.LOCATION}"
     tags = ["vpn"]
-    hostname = "vpn-${var.LOCATION}"
+    hostname = "${var.LOCATION}"
     enable_ipv6 = false
     disable_public_ipv4 = false
-    backups = "enabled"
-    backups_schedule {
-            type = "daily"
-    }
-    ddos_protection = true
+    backups = "disabled"
+    ddos_protection = false
     activation_email = false
+    user_data = templatefile("${path.module}/ignition.tftpl.json", {
+      hostname = var.LOCATION
+      os_user = var.USERNAME
+      ssh_key = var.SSH_KEY
+    })   
+}
+
+# 3. Output 
+output "instance_details" {
+  description = "Details of the Instance"
+  value = {
+    ip = vultr_instance.vpn.main_ip
+    os = vultr_instance.vpn.os
+    region = vultr_instance.vpn.region
+    status = vultr_instance.vpn.status
+    hostname = vultr_instance.vpn.hostname
+  }
 }
